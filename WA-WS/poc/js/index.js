e32b9f6df4a36049b686f5e9223def4b5c618320
Jackson <jmax825@uw.edu>
2017-08-15 16:31:14 -0700

    working dropdown for sorting committee events

--- WA-WS/poc/js/index.js
+++ WA-WS/poc/js/index.js
@@ -25,7 +25,7 @@ function makeCORSRequest(url, target_function, target) {
       let store_time = new Date()
       localStorage[target + '_last_stored'] = store_time.toString();
       localStorage[target] = JSON.stringify(data);
-      target_function(data);
+      target_function(data, '*');
     },
     error: function(e) {
       console.log(e);
@@ -34,11 +34,34 @@ function makeCORSRequest(url, target_function, target) {
   });
 };
 
+function returnDataFromCORSRequest(url, store_name) {
+  $.ajax(url, {
+    type: 'GET',
+    contentType: 'text/plain',
+    dataType: 'jsonp',
+    xhrFields: {
+      withCredentials: true
+    },
+    headers: {
+
+    },
+    success: function(data) {
+      let store_time = new Date()
+      localStorage[store_name + '_last_stored'] = store_time.toString();
+      localStorage[store_name] = JSON.stringify(data);
+      return data;
+    },
+    error: function(e) {
+      console.log('Error getting', store_name, ':', e);
+    }
+  });
+};
+
 function createErrorObjects(target) {
 
 };
 
-function loadData(url, target, target_function) {
+function loadData(url, target, target_function, returnData) {
   let events_last_stored = localStorage[target + '_last_stored'];
   events_last_stored = Date.parse(events_last_stored);
   events_last_stored = new Date(events_last_stored);
@@ -49,32 +72,50 @@ function loadData(url, target, target_function) {
   console.log('Minutes sinces last ' + target + ' collection:', difference);
 
   if (isNaN(difference) || difference >= 30) {
-    makeCORSRequest(url, target_function, target);
+    if (returnData) {
+      returnDataFromCORSRequest(url, target)
+    } else {
+      makeCORSRequest(url, target_function, target);
+    }
   } else {
-    const events_stored = localStorage[target];
-    const events_data = JSON.parse(events_stored);
-    target_function(events_data)
+    if (returnData) {
+      let data_stored = localStorage[target];
+      data_stored = JSON.parse(data_stored);
+      return data_stored
+    } else {
+      const events_stored = localStorage[target];
+      const events_data = JSON.parse(events_stored);
+      target_function(events_data, '*')
+    }
   }
-}
-
-function loadAll() {
-  loadData('http://webapi.legistar.com/v1/seattle/Events', 'recent_events', createRecentEventsObjects)
-
-  makeCORSRequest('http://webapi.legistar.com/v1/seattle/OfficeRecords', createIndexObjects, 'indexes')
 };
 
-function createRecentEventsObjects(data) {
-  const eventData_5 = alasql('SELECT TOP 5 * FROM ? ORDER BY EventDate DESC', [data]);
+function loadAll() {
+  loadData('http://webapi.legistar.com/v1/seattle/Events', 'recent_events', createRecentEventsObjects, false)
 
-  console.log('5 recent events:', eventData_5);
+  //makeCORSRequest('http://webapi.legistar.com/v1/seattle/Bodies', createIndexObjects, 'indexes')
+};
 
-  eventData_5.forEach((event) => {
+function createRecentEventsObjects(data, where) {
+  let event_data_5;
+
+  if (where == '*') {
+    event_data_5 = alasql('SELECT TOP 5 * FROM ? ORDER BY EventDate DESC', [data]);
+  } else {
+    event_data_5 = alasql('SELECT TOP 5 * FROM ? WHERE EventBodyName="' + where + '" ORDER BY EventDate DESC', [data]);
+  }
+
+  console.log('5 recent events:', event_data_5);
+
+  $('#recent_events_data').children().remove();
+
+  event_data_5.forEach((event) => {
     let event_date = Date.parse(event.EventDate);
     event_date = new Date(event_date);
 
     let event_time = event.EventTime;
 
-    $('#recent_events').append(
+    $('#recent_events_data').append(
       $('<div/>')
       .attr('id', 'event_' + event.EventId)
       .on('click', () => {
@@ -105,4 +146,11 @@ function createRecentEventsObjects(data) {
 
 function createIndexObjects(data) {
   console.log(data);
+};
+
+function updateListData(where, target_data, target_function) {
+  let json_target_data = localStorage[target_data];
+  json_target_data = JSON.parse(json_target_data);
+
+  target_function(json_target_data, where);
 };
